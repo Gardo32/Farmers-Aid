78d3d0e22dd2e0f288554da522eeb126e5ec5cb6
Gardo32 <nv22084@nvtc.edu.bh>
2024-09-25 10:43:47 +0300

    Fixed requirements

--- deps.py
+++
@@ -1,8 +0,0 @@
-import os
-
-deps = ['streamlit', 'pandas','requests','python-dotenv','plotly','pycountry']
-
-os.system("pip install uv")
-
-for dep in deps:
-    os.system("uv pip install " + dep)
